10990c34542616b518542ef1c2a8f7bb23a21c81
dev Anderson Roberto <ander9854@gmail.com>
2024-01-22 18:56:09 -0300

    alteração responsividade 3.4v

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,15 @@ setInterval( function() {
     nextSlide()
 }, 5000)
 
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+    }else {
+        menuMobile.classList.add('open')
+    }
+}
+
 function nextSlide(){
     count++;
     if(count>5){
