e27c731b4e06c61790f6a5b58fdd07f85c641d22
dev Anderson Roberto <ander9854@gmail.com>
2024-02-17 09:57:39 -0300

    alteração 3.4.5.2v

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 let count = 1;
+
+const indicator = document.querySelector('#indicator')
+
+const maxheight = document.body.scrollHeight - window.innerHeight;
+
+window.addEventListener('scroll', () => {
+    const percentage = (window.scrollY / maxheight) * 100;
+
+    indicator.style.width = `${percentage}%`;
+})
+
 document.getElementById("slide1").checked = true;
+
 
 setInterval( function() {
     nextSlide()
@@ -22,3 +34,40 @@ function nextSlide(){
 
     document.getElementById("slide" + count).checked = true;
 }
+
+const slider = document.querySelectorAll('.sliders');
+const btnPrev = document.getElementById('down');
+const btnNext = document.getElementById('up');
+
+let currentSlide = 0;
+
+function hideSlider() {
+    slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+    slider[currentSlide].classList.add('on')
+}
+
+function NextSlider() {
+    hideSlider()
+    if(currentSlide === slider.length -1) {
+    currentSlide = 0
+    } else {
+    currentSlide++
+    }
+    showSlider()
+}
+
+function prevSlider() {
+    hideSlider()
+    if(currentSlide === 0) {
+    currentSlide = slider.length -1
+    } else {
+    currentSlide--
+    }
+    showSlider()
+}
+
+btnNext.addEventListener('click', NextSlider)
+btnPrev.addEventListener('click', prevSlider)
